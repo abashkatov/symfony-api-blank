f4376a02c008390dea6278e8b0f72f010add525c
Aleksey Bashkatov <a.bashkatov@corp.vk.com>
2022-03-04 22:45:12 +0300

    adds security, user entity, migration

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    #[Route('/registration', name: 'app_registration', methods: 'POST')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/AuthController.php',
+        ]);
+    }
+}
---
+++ src/Enum/Role.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enum;
+
+class Role
+{
+    public const USER = 'ROLE_USER';
+}
